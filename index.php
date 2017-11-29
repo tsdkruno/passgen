5ca7fd369c70e7426f4ae4cdf3e318e04e21c91f
Krunoslav Husak <krunoslav.husak@skole.hr>
2017-11-29 13:31:37 +0100

    Izmjenjen index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css"></link>
+    </head>
+
+    <body>
+        <h1>
+            Password generator
+        </h1>
+
+        <form action="passgen.php" method="post">
+            <input type="text" name="broj_znakova"> broj znakova<br>
+            <input type="checkbox" name="brojevi"> brojevi<br>
+            <input type="checkbox" name="posebni"> posebni znakovi<br>
+            <input type="submit" value="Generiraj!">
+        </form>
+    </body>
+</html>
